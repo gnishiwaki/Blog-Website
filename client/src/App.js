804a3b86d6ca2b03642029fcf8e6b69df1f040fc
gnishiwaki <gen.nishiwaki@gmail.com>
2021-06-02 12:16:30 -0400

    Set up the styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
-import memories from './images/memories.png'
+import memories from './images/memories1.png'
+import useStyles from './styles';
+
 const App = () => {
+    const classes=useStyles();
+
     return (
         <Container maxidth="lg">
-            <AppBar position = "static" color = "inherit">
-                <Typography variant="h2" align = "center">Memories</Typography>
-                <img src={memories} alt="memories" height="60"/>
+            <AppBar className={classes.appBar} position = "static" color = "inherit">
+                <Typography className={classes.heading} variant="h2" align = "center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
